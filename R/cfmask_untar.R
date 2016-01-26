020e25152e8f3bda507be2cb7964ba6b7a5d13a1
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-26 12:17:02 +0100

    Completed function to select images with low cloud cover

--- R/cfmask_untar.R
+++ R/cfmask_untar.R
@@ -8,6 +8,10 @@ library(rgdal)
 ## setting variables
 tarList <- list("data/LT51940282001212-SC20160125050334.tar.gz")
 extentArea <- extent(447315, 458985, 5156955, 5169255)
+extent_raster <- raster(nrows=410, ncols=389,ext=extentArea)
+FreqEA <- freq(extent_raster, value=NA, useNA="always", progress="text")
+
+
 
 #CloudCheck <- function(LSyear) {
 	for (folder in tarList[]) {
@@ -18,32 +22,16 @@ extentArea <- extent(447315, 458985, 5156955, 5169255)
 		## crop CFmask
 		CFmask_crop <- crop(CFmask, extentArea)
 		## calculating cloud cover area
-		CFmask_crop[CFmask_crop != 4] <- NA
-		plot(CFmask_crop)
-		
-	
-		
-		cell_size <- area(CFmask_crop, filename="data/CloudTemp/cell_size", na.rm=TRUE, weights=FALSE)
-		cell_size <- cell_size[!is.na(cell_size)]
-		cloud_area <- length(cell_size)*median(cell_size)
-		print(paste("Area of cloud regions (0-999 m):",round(cloud_area, digits=1),"km2"))
-		extent_raster <- raster(extentArea)
-		plot(extent_raster)
-		cell_size_ext <- area(extent_raster, filename="data/CloudTemp/cell_size_ext", na.rm=FALSE, weights=FALSE)
-		cell_size_ext <- cell_size_ext[!is.na(cell_size_ext)]
-		extent_area <- length(cell_size_ext)*median(cell_size_ext)
-		
-		a <- (cloud_area/extent_area)
-		
-		plot(CFmask_crop)
-		
-		
-		
-	}
-cfmask_untar <- function(folder_path, LSyear) {
-	
-
-
-
-untar("data/LT51940282001212-SC20160125050334.tar.gz", files=glob2rx("LT5*cfmask.tif") ,exdir = paste("data/", "LSyear"))
-untar("data/LT51940282001212-SC20160125050334.tar.gz", exdir = paste("data/CloudTemp"))
+		FreqCM <-freq(CFmask_crop, digits=0, value=4, useNA='no', progress="text")
+		Cloud_area <- (FreqCM/FreqEA)*100
+		## keeping only images with low cloud cover in cropped area
+		if (Cloud_area < 20) {
+			untar(folder, exdir = "data")
+			print("Cloud cover is below 20% and the files will be removed")
+		}
+		else {
+			print("Cloud cover is above 20% and the files will be removed")
+		}
+		list_del <- list.files("data/CloudTemp", full.names = TRUE)
+		unlink(list_del)
+}
