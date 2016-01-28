dd724c42b31e9c9087f250cb9fad518deb2bfc19
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-28 14:21:50 +0100

    Clean and documented version of the script

--- main.R
+++ main.R
@@ -1,51 +1,27 @@
 ## Author: The Leopards (Samantha Krawczyk, Georgios Anastasiou)
 ## 28 January 2016
-## Comparing and visualising change in 2 Landsat images of the Rhone Glacier, CH
+## Comparing and visualising change in Landsat images of the Rhone Glacier, Switzerland
+## Glacier extent estimated using NDSI (Normalised Differentiated Snow Index)
+## The method is validated through comparison with GLIMS dataset
 
-## libraries
-library(sp)
-library(raster)
-
-
-#setwd("..../")
-getwd()
-
-## checking cloud cover for area of interest, if cloud cover is less than 20%, it untars 
-## list of downloaded tar files
-
-##"data/LT51940282009218-SC20160127034452.tar.gz", "data/LT51940282001212-SC20160125050334.tar.gz", "data/LT51940282003234-SC20160126032814.tar.gz", "data/LT51940282010221-SC20160125050415.tar.gz", "data/LT51940282011240-SC20160126032704.tar.gz"
-
-tarList <- list("data/LT51940282009250-SC20160127034608.tar.gz")
-source("R/ImageSelection.R")
+## checking cloud cover for area of interest; if cloud cover is less than 50% the file is untared
+tarList <- list("data/LT51940282009218-SC20160127034452.tar.gz", "data/LT51940282009250-SC20160127034608.tar.gz", "data/LT51940282001212-SC20160125050334.tar.gz", "data/LT51940282003234-SC20160126032814.tar.gz", "data/LT51940282010221-SC20160125050415.tar.gz", "data/LT51940282011240-SC20160126032704.tar.gz")
+source("R/image_selection.R")
 ImageSelection()
 
-#ImageList <- (list.files("data", pattern = "*.grd", full.names = TRUE))
-source("R/CloudMask.R")
-CloudMask()
+## masking the images for water and cloud areas derived from Landsat layer CFmask
+source("R/cloud_water_mask.R")
+cloud_water_mask()
 
-
-source("R/Calc_NDSI.R")
+## calculating NDSI
+source("R/calc_NDSI.R")
 Calc_NDSI()
 
-source("R/Visualisation_NDSI.r") #- good, but rename with .R
+## plotting all glacier areas derived from NDSI
+source("R/glacier_area_plot.R")
 
-source("R/NDSI_overlay.R") #- under construction
-# Plot the stack and the cloud mask on top of each other
-#plotRGB(RhoneG, 1,2,3)
-#plot(cloud, add = TRUE, legend = FALSE)
-#plot(RhoneG, 5, add = TRUE, legend = TRUE)
+## plotting glacier outlines on top of each other
+source("R/NDSI_outline_overlay.R")
 
- 
-
-
-#trying to get rid of values<0 to be able to do plotRGB
-#RhoneG4["LT51940282010221MOR00_sr_band5" < 0] <- NA
-
-# Visualize the output
-#plotRGB(RhoneG4, 1,2,3)
-
-
-
-
-
-
+## validating the use of NDSI as a proxy for glacier extent by comparing it with the GLIMS dataset
+source("R/NDSI_validation.R")
